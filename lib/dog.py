7230e1fdb75bcb6158109c3df7a3a58cbda90a87
Denzel <jdenzelmab@gmail.com>
2023-07-11 19:05:20 -0400

    Completed instance methods lab

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,16 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
 
-    #Instance method definition
-    pass
+    #Instance method definition 
+fido = Dog()
+fido.bark()
+snoopy = Dog()
+snoopy.bark()
+fido.sit()
+
